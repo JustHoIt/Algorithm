5f5e77001ccb6cbd9c413f0f8dfd293b19cdf56c
Park <zalop3128@kakao.com>
2022-08-09 05:48:50 +0900

    2208009

---
+++ src/String/Main11_2.java
@@ -0,0 +1,34 @@
+package String;
+
+import java.util.Scanner;
+
+public class Main11_2 {
+    public String solution(String str) { //KKHSSSSSSE
+        String answer = "";
+        str += " ";  // for j 문에서 str.length()값이되면 비교할 i+1이 없어서 오류가 생겨서 임시로 생성하기
+
+        for (int i = 0; i < str.length()-1; i++) {
+            char c = str.charAt(i); // i = 0 : c = K /
+            int count = 1;
+            for (int j = i+ 1; j < str.length(); j++) {
+                if(c == str.charAt(j)){
+                    count++;
+                    i++;
+                }else{
+                    answer += str.charAt(i) + String.valueOf(count);
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main11_2 T = new Main11_2();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+
+
+    }
+}
---
+++ src/String/Main9_1.java
@@ -0,0 +1,22 @@
+package String;
+
+import java.util.Scanner;
+
+public class Main9_1 {
+    public int solution(String str) {
+        int answer = 0;
+        for (char x : str.toCharArray()) {
+            if (x >= 48 && x <= 57) {
+                answer = answer * 10 + (x - 48);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main9_1 T = new Main9_1();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
