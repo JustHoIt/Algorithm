d7ef9dbe259d606d9c37462735294df3e3f72c6d
Park <zalop3128@kakao.com>
2023-10-31 18:57:04 +0900

    [BAEKJOON-2728]Title : 피보나치 수, Time-108ms / Memory-12868kb

--- src/hashmaptreeset/Main1_1.java
+++ src/hashmaptreeset/Main1_1.java
@@ -1,26 +1,23 @@
-package HashMapTreeSet;
+package hashmaptreeset;
 
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main1_1 {
     public char solution(int n, String s) {
-        char answer=' ';
+        char answer = ' ';
         HashMap<Character, Integer> map = new HashMap<>();
-        for(char x : s.toCharArray()) {
-            map.put(x,map.getOrDefault(x, 0)+1);
+        for (char x : s.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
         int max = Integer.MIN_VALUE;
-        for(char key : map.keySet()){
-            if(map.get(key)> max) {
+        for (char key : map.keySet()) {
+            if (map.get(key) > max) {
                 max = map.get(key);
                 answer = key;
             }
         }
-
-
-
         return answer;
 
     }
